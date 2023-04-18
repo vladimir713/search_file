04817330ebca041bf39e5d201894c8a44e3ef965
vladimir713 <vladimir713@mail.ru>
2023-04-19 00:09:21 +0300

    Создал пулл потоков

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        searchFile("system.ini", "c:\\Dell");
+        searchFile("system.ini", "c:\\Intel");
     }
     public static void searchFile(String file, String dir) throws IOException {
         List<Path> listDirs = Files.walk(Paths.get(dir))
-                .filter(Files::isDirectory)
-                .collect(Collectors.toList());
+                .filter(Files::isDirectory).toList();
         for(Path s:listDirs) {
             System.out.println(s.toString());
         }
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (Path p:listDirs) {
+            executorService.execute(() -> System.out.println("Поток"));
+        }
+        executorService.shutdown();
     }
 }
