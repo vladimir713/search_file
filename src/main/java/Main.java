aef4300525e6e90a1c0d27683bb1c7de77b80973
vladimir713 <vladimir713@mail.ru>
2023-04-18 23:41:42 +0300

    Получил список директорий

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        searchFile("system.ini", "c:\\Dell");
+    }
+    public static void searchFile(String file, String dir) throws IOException {
+        List<Path> listDirs = Files.walk(Paths.get(dir))
+                .filter(Files::isDirectory)
+                .collect(Collectors.toList());
+        for(Path s:listDirs) {
+            System.out.println(s.toString());
+        }
     }
 }
