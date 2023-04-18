592d71b9b33464823180c3009dbe3de864e35b39
vladimir713 <vladimir713@mail.ru>
2023-04-19 00:31:43 +0300

    Многопоточный вывод всех файлов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        searchFile("system.ini", "c:\\Intel");
+        searchFile("system.ini", "c:\\Windows\\Help");
     }
     public static void searchFile(String file, String dir) throws IOException {
         List<Path> listDirs = Files.walk(Paths.get(dir))
                 .filter(Files::isDirectory).toList();
         for(Path s:listDirs) {
-            System.out.println(s.toString());
+            System.out.println(s);
         }
         ExecutorService executorService = Executors.newCachedThreadPool();
+
         for (Path p:listDirs) {
-            executorService.execute(() -> System.out.println("Поток"));
+            executorService.execute(() -> {
+                try {
+                    List<Path> listFiles = Files.walk(Paths.get(p.toString()))
+                            .filter(Files::isRegularFile).toList();
+                    for (Path f: listFiles) {
+                        System.out.println(f);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
         executorService.shutdown();
     }
